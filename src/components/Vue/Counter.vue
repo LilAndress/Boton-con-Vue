3fc8fe725ed9f8c463d8d66814e3284304d68847
Andres Rojas <afrojasca@ufpso.edu.co>
2024-04-25 12:59:30 -0500

    Boton con Vue

---
+++ src/components/Vue/Counter.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'Vue' ;
+
+let count = ref(0);
+
+function addvalue() {
+    count.value++;
+    console.log(count)
+}
+</script>
+
+
+<template>
+  <div @click="addvalue" class="bg-blue-500 text-white py-2 rounded hover:cursor-pointer hover:bg-blue-600 transition-colors inline-block">
+    ¡Presioname!{{ count }}
+  </div>
+
+</template>
